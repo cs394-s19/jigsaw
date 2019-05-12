049ffa04a212deaef2148a2dcaf4bcf7a57d6fa1
afkim14 <andreskim2018@u.northwestern.edu>
2019-05-11 22:36:18 -0500

    tab navigation

--- jigsaw/navigation/MainTabNavigator.js
+++ jigsaw/navigation/MainTabNavigator.js
@@ -7,54 +7,150 @@ import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
-});
+import CalendarScreen from '../screens/CalendarScreen';
+import CreateEventScreen from '../screens/CreateEventScreen';
+import EventsScreen from '../screens/EventsScreen';
+import RequestsScreen from '../screens/RequestsScreen';
+import AccountScreen from '../screens/AccountScreen';
 
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
+const CalendarStack = createStackNavigator({
+  Calendar: CalendarScreen,
+});
+CalendarStack.navigationOptions = {
+  tabBarLabel: 'Calendar',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
       name={
         Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
+          ? `ios-calendar`
+          : 'md-calendar'
       }
     />
   ),
-};
+}
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
+const CreateEventStack = createStackNavigator({
+  CreateEvent: CreateEventScreen,
 });
-
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+CreateEventStack.navigationOptions = {
+  tabBarLabel: 'Create Event',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-create`
+          : 'md-create'
+      }
     />
   ),
-};
+}
 
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
+const EventsStack = createStackNavigator({
+  Events: EventsScreen,
 });
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
+EventsStack.navigationOptions = {
+  tabBarLabel: 'Events',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-list`
+          : 'md-list'
+      }
     />
   ),
-};
+}
+
+const RequestsStack = createStackNavigator({
+  Requests: RequestsScreen,
+});
+RequestsStack.navigationOptions = {
+  tabBarLabel: 'Requests',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-notifications`
+          : 'md-notifications'
+      }
+    />
+  ),
+}
+
+const AccountStack = createStackNavigator({
+  Requests: AccountScreen,
+});
+AccountStack.navigationOptions = {
+  tabBarLabel: 'Account',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-contact`
+          : 'md-contact'
+      }
+    />
+  ),
+}
+
+// const HomeStack = createStackNavigator({
+//   Home: HomeScreen,
+// });
+//
+// HomeStack.navigationOptions = {
+//   tabBarLabel: 'Home',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={
+//         Platform.OS === 'ios'
+//           ? `ios-information-circle${focused ? '' : '-outline'}`
+//           : 'md-information-circle'
+//       }
+//     />
+//   ),
+// };
+//
+// const LinksStack = createStackNavigator({
+//   Links: LinksScreen,
+// });
+//
+// LinksStack.navigationOptions = {
+//   tabBarLabel: 'Links',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+//     />
+//   ),
+// };
+//
+// const SettingsStack = createStackNavigator({
+//   Settings: SettingsScreen,
+// });
+//
+// SettingsStack.navigationOptions = {
+//   tabBarLabel: 'Settings',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+//     />
+//   ),
+// };
 
 export default createBottomTabNavigator({
-  HomeStack,
-  LinksStack,
-  SettingsStack,
+  CalendarStack,
+  CreateEventStack,
+  EventsStack,
+  RequestsStack,
+  AccountStack,
+  // HomeStack,
+  // LinksStack,
+  // SettingsStack,
 });
