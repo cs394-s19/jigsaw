a2dca4e00aff6b6803c874cd28ec35f98980f1ee
juztinfied <z_d_143@hotmail.com>
2019-05-12 23:46:37 -0500

    adjust navigators abit

--- jigsaw/navigation/AppNavigator.js
+++ jigsaw/navigation/AppNavigator.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { createSwitchNavigator } from 'react-navigation';
-
 import MainTabNavigator from './MainTabNavigator';
 
-export default createSwitchNavigator({
+
+const MainNav = createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
 });
+
+export default class AppNavigator extends React.Component {
+  render() {
+    console.log('inside appnavigator');
+    console.log(this.props);
+    return(<MainNav screenProps={{data: this.props.data}} />)
+  }
+}
--- jigsaw/navigation/MainTabNavigator.js
+++ jigsaw/navigation/MainTabNavigator.js
@@ -98,59 +98,18 @@ AccountStack.navigationOptions = {
   ),
 }
 
-// const HomeStack = createStackNavigator({
-//   Home: HomeScreen,
-// });
-//
-// HomeStack.navigationOptions = {
-//   tabBarLabel: 'Home',
-//   tabBarIcon: ({ focused }) => (
-//     <TabBarIcon
-//       focused={focused}
-//       name={
-//         Platform.OS === 'ios'
-//           ? `ios-information-circle${focused ? '' : '-outline'}`
-//           : 'md-information-circle'
-//       }
-//     />
-//   ),
-// };
-//
-// const LinksStack = createStackNavigator({
-//   Links: LinksScreen,
-// });
-//
-// LinksStack.navigationOptions = {
-//   tabBarLabel: 'Links',
-//   tabBarIcon: ({ focused }) => (
-//     <TabBarIcon
-//       focused={focused}
-//       name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
-//     />
-//   ),
-// };
-//
-// const SettingsStack = createStackNavigator({
-//   Settings: SettingsScreen,
-// });
-//
-// SettingsStack.navigationOptions = {
-//   tabBarLabel: 'Settings',
-//   tabBarIcon: ({ focused }) => (
-//     <TabBarIcon
-//       focused={focused}
-//       name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-//     />
-//   ),
-// };
-
-export default createBottomTabNavigator({
+const MainBtmTabNav = createBottomTabNavigator({
   CalendarStack,
   CreateEventStack,
   EventsStack,
   RequestsStack,
   AccountStack,
-  // HomeStack,
-  // LinksStack,
-  // SettingsStack,
 });
+
+export default class MainTabNavigator extends React.Component {
+  render() {
+    console.log('inside main tab navigator');
+    console.log(this.props);
+    return(<MainBtmTabNav />)
+  }
+}
