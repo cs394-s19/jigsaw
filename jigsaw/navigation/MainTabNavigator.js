02e4d7eaa2edc813d42c34fdb23f2da1b449c69b
juztinfied <z_d_143@hotmail.com>
2019-05-12 23:50:42 -0500

    cleaning justin2 branch

--- jigsaw/navigation/MainTabNavigator.js
+++ jigsaw/navigation/MainTabNavigator.js
@@ -12,7 +12,7 @@ import CreateEventScreen from '../screens/CreateEventScreen';
 import EventsScreen from '../screens/EventsScreen';
 import RequestsScreen from '../screens/RequestsScreen';
 import AccountScreen from '../screens/AccountScreen';
-//
+
 
 const CalendarStack = createStackNavigator({
   Calendar: CalendarScreen,
@@ -100,59 +100,10 @@ AccountStack.navigationOptions = {
   ),
 }
 
-// const HomeStack = createStackNavigator({
-//   Home: HomeScreen,
-// });
-//
-// HomeStack.navigationOptions = {
-//   tabBarLabel: 'Home',
-//   tabBarIcon: ({ focused }) => (
-//     <TabBarIcon
-//       focused={focused}
-//       name={
-//         Platform.OS === 'ios'
-//           ? `ios-information-circle${focused ? '' : '-outline'}`
-//           : 'md-information-circle'
-//       }
-//     />
-//   ),
-// };
-//
-// const LinksStack = createStackNavigator({
-//   Links: LinksScreen,
-// });
-//
-// LinksStack.navigationOptions = {
-//   tabBarLabel: 'Links',
-//   tabBarIcon: ({ focused }) => (
-//     <TabBarIcon
-//       focused={focused}
-//       name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
-//     />
-//   ),
-// };
-//
-// const SettingsStack = createStackNavigator({
-//   Settings: SettingsScreen,
-// });
-//
-// SettingsStack.navigationOptions = {
-//   tabBarLabel: 'Settings',
-//   tabBarIcon: ({ focused }) => (
-//     <TabBarIcon
-//       focused={focused}
-//       name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-//     />
-//   ),
-// };
-
 export default createBottomTabNavigator({
   CalendarStack,
   CreateEventStack,
   EventsStack,
   RequestsStack,
   AccountStack,
-  // HomeStack,
-  // LinksStack,
-  // SettingsStack,
 });
