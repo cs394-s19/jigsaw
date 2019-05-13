033a26f85d9aab4b029ab4c99c281cec4ab37a42
David Wolff <davidwolff2020@u.northwestern.edu>
2019-05-13 17:08:08 -0500

    p2

--- jigsaw/constants/Scheduler.js
+++ jigsaw/constants/Scheduler.js
@@ -1,8 +1,19 @@
-var hash =
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-var getFreeTimes = (users) => {
+var map = {};
+var makeHashMap = (range) => {
 
-  // iterate through each user
+  var inc = 30;
+
+  for(var i = 1; i<8; i++){
+    for(var j = 0; j<23; j++){
+      for(var k = 0; k<2; k++){
+        var hash = i+":"+j+":"+(k*inc);
+        map[hash] = 0;
+        // console.log(hash);
+      }
+    }
+  }
 };
 
 export function schedule(users, preferences) {
