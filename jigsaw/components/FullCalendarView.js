388aec6244cb19a4be3da68171ab8e77697fc328
Allan <allanfarkash2020@u.northwestern.edu>
2019-06-02 22:56:32 -0500

    FullCalendarView is pretty much done!!

--- jigsaw/components/FullCalendarView.js
+++ jigsaw/components/FullCalendarView.js
@@ -11,6 +11,7 @@ import { Ionicons } from '@expo/vector-icons';
 export default class FullCalendarView extends Component {
   state = {
     schedule: [],
+    items: {}
   };
 
   componentDidMount() {
@@ -26,49 +27,31 @@ export default class FullCalendarView extends Component {
   }
 
   /*renderEventDay(day) {
-    console.log
-    var todaysEvents = [];
-    //const sched = JSON.parse(this.state.schedule);
-    for (event in this.state.schedule){
-        if (event.Day == this.myGetDay(day)){
-          todaysEvents.push(event);
-        }
-      }
-    for (e in todaysEvents){
-      /*return(
-        <TouchableOpacity style={styles.eventContainer}>
-          <Text style={styles.eventTitle}>{e["Name"]}</Text>
-          <Text style={styles.eventTime}>{e["Start"]} - {e["End"]}</Text>
-        </TouchableOpacity>
-
-      );
-      console.log(e);
-    }
-    if (todaysEvents == []){
-      return null;
-    }
-  }*/
-
-  renderEventDay(day) {
-    var allEvents = [];
     return this.state.schedule.map((event) => {
-      if (event["Day"] == this.myGetDay(day)) {
-        return (day.dateString + " : [{text: " + event["Name"] + "}]");
+      if (event["Day"] == this.myGetDay(day)) { 
+      
+        return event;
       }
       else{
         return null;
-      }
-        //'2019-05-22': [{text: 'item 1 - any js object'}],
-        /*return (
-          <TouchableOpacity key={index} style={styles.eventContainer}>
-            <Text style={styles.eventTitle}>{event["Name"]}</Text>
-            <Text style={styles.eventTime}>{event["Start"]} - {event["End"]}</Text>
-          </TouchableOpacity>*/
-        
+        }
       }
     );
-    //console.log(allEvents);
   }
+
+  createItems(day, events){
+    var allEvents = events.filter(function (el) {
+      return el != null;
+    });
+    itemList = [];
+    
+    for(i=0; i<allEvents.length; i++){
+      if (allEvents[i] != null){
+      itemList.push(allEvents[i]);
+      }
+    }
+    return this.state.items[day.dateString] = itemList;
+  }*/
 
   myGetDay(day) {
     var days = [
@@ -81,92 +64,99 @@ export default class FullCalendarView extends Component {
       'Sunday'
     ];
 
-    date = new Date(day.dateString);
+    date = new Date(day);
     return days[date.getDay()];
   }
-  
-  
-  
+
   render() {
     return (
       <Agenda
-  // the list of items that have to be displayed in agenda. If you want to render item as empty date
-  // the value of date key kas to be an empty array []. If there exists no value for date key it is
-  // considered that the date in question is not yet loaded
-  items={{
-    '2019-05-22': [{text: 'item 1 - any js object'}],
-    '2019-05-23': [{text: 'item 2 - any js object'}],
-    '2019-05-24': [],
-    '2019-05-25': [{text: 'item 3 - any js object'},{text: 'any js object'}]
-  }}
-  // callback that gets called when items for a certain month should be loaded (month became visible)
-  loadItemsForMonth={(month) => {console.log('trigger items loading')}}
-  // callback that fires when the calendar is opened or closed
-  onCalendarToggled={(calendarOpened) => {console.log(calendarOpened)}}
-  // callback that gets called on day press
-  onDayPress={(day)=>{console.log(this.renderEventDay(day));}}
-  // callback that gets called when day changes while scrolling agenda list
-  onDayChange={(day)=>{console.log('day changed')}}
-  // initially selected day
-  selected={new Date().toISOString().slice(0, 10)}
-  // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-  minDate={'2018-05-10'}
-  // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-  maxDate={'2025-05-30'}
-  // Max amount of months allowed to scroll to the past. Default = 50
-  pastScrollRange={50}
-  // Max amount of months allowed to scroll to the future. Default = 50
-  futureScrollRange={50}
-  // specify how each item should be rendered in agenda
-  renderItem={(item, firstItemInDay) => {return (<View />);}}
-  // specify how each date should be rendered. day can be undefined if the item is not first in that day.
-  renderDay={(day, item) => {return this.renderEventDay(day);}}
-  // specify how empty date content with no items should be rendered
-  renderEmptyDate={() => {return (<View />);}}
-  // specify how agenda knob should look like
-  renderKnob={() => {return (<Ionicons name="ios-arrow-down" size={32} color="goldenrod" />);}}
-  // specify what should be rendered instead of ActivityIndicator
-  renderEmptyData = {() => {return (<View />);}}
-  // specify your item comparison function for increased performance
-  rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
-  // Hide knob button. Default = false
-  hideKnob={false}
-  // By default, agenda dates are marked if they have at least one item, but you can override this if needed
-  markedDates={{
-    '2019-05-16': {selected: true, marked: true},
-    '2019-05-17': {marked: true},
-    '2019-05-18': {disabled: true}
-  }}
-  // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
-  onRefresh={() => console.log('refreshing...')}
-  // Set this true while waiting for new data from a refresh
-  refreshing={false}
-  // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
-  refreshControl={null}
-  // agenda theme
-  theme={{
-    //...calendarTheme,
-    agendaDayTextColor: 'yellow',
-    agendaDayNumColor: 'green',
-    agendaTodayColor: 'red',
-    agendaKnobColor: 'blue'
-  }}
-
-  // agenda container style
-  style={{}}
-/>
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        selected={new Date().toISOString().slice(0, 10)}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+      />
     );
   }
-}
-      
 
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+      let events = [];
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+
+        events = this.state.schedule.map((event) => {
+          if (event["Day"] == this.myGetDay(strTime)) {           
+            return event;
+          }
+          else{
+            return null;
+            }
+          });
+
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          var allEvents = events.filter(function (el) {
+            return el != null;
+          });
+          itemList = [];
+          
+          for(i=0; i<allEvents.length; i++){
+            if (allEvents[i] != null){
+              this.state.items[strTime].push(allEvents[i]);
+            }
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+  }
+
+  renderItem(item) {
+    return (
+      <View style={[styles.item]}>            
+        <Text style={styles.eventTitle}>{item.Name}</Text>
+        <Text style={styles.eventTime}>{item.Start} - {item.End}</Text>
+      </View>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}><Text>Nothing planned for today!</Text></View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
+}
 
 const styles = StyleSheet.create({
-  container: {
+  item: {
+    backgroundColor: 'white',
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center'
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17,
+    height: 100
   },
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
+  }
 });
